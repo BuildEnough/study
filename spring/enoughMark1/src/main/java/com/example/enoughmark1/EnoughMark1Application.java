80e688e0706727f888eda53cf15a9810812ad9c6
kbw <rhtmaehcl878@naver.com>
2025-11-17 18:33:02 +0900

    웹 사이트 호스팅 테스트 프로젝트

---
+++ spring/enoughMark1/src/main/java/com/example/enoughmark1/EnoughMark1Application.java
@@ -0,0 +1,13 @@
+package com.example.enoughmark1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnoughMark1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnoughMark1Application.class, args);
+    }
+
+}
---
+++ spring/enoughMark1/src/test/java/com/example/enoughmark1/EnoughMark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enoughmark1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnoughMark1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
