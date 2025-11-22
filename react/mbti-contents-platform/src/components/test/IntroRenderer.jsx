2de17055da51e4e0f2bc55e4a440bb423a4551f9
kbw <rhtmaehcl878@naver.com>
2025-11-22 18:14:48 +0900

    퀴즈 페이지 생성

--- react/mbti-contents-platform/src/components/test/IntroRenderer.jsx
+++ react/mbti-contents-platform/src/components/test/IntroRenderer.jsx
@@ -4,11 +4,28 @@ import Quiz from "./Quiz";
 import Loading from "./Loading";
 
 function IntroRenderer({ currentTest }) {
+        const [mbtiScore, setMbtiScore] = useState( {
+            E: 0,
+            I: 0,
+            S: 0,
+            N: 0,
+            T: 0,
+            F: 0,
+            J: 0,
+            P: 0,
+        });
         const [mode, setMode] = useState("intro");
         if(mode === 'intro') {
             return <Intro info={currentTest?.info} setMode={setMode} />;
         } else if (mode === 'quiz') {
-            return <Quiz />;
+            return (
+                <Quiz 
+                    setMode={setMode}
+                    questions={currentTest?.questions}
+                    mbtiScore={mbtiScore}
+                    setMbtiScore={setMbtiScore}
+                />
+            );
         } else if (mode === 'loading') {
             return <Loading />;
         } else {
--- react/mbti-contents-platform/src/components/test/Quiz.jsx
+++ react/mbti-contents-platform/src/components/test/Quiz.jsx
@@ -1,5 +1,18 @@
-function Quiz() {
-    return <div>Quiz</div>;
+import { useState } from "react";
+
+function Quiz({setMode, questions, mbtiScore, setMbtiScore}) {
+    const [questionNum, setQuestionNum] = useState(0);
+    const onOptionClick = () => {
+        setQuestionNum((prev) => prev + 1);
+    }
+    return <div>
+        <h3>{questions[questionNum]?.question}</h3>
+        <button onClick={onOptionClick}>
+            {questions[questionNum]?.answers[0]?.content}</button>
+        <button onClick={onOptionClick}>
+            {questions[questionNum]?.answers[1]?.content}</button>
+        <div>프로그레스 바</div>
+    </div>;
 }
 
 export default Quiz;
