206d9503a8fb9f34fb5b1b53e4093c35e66e4e72
kbw <rhtmaehcl878@naver.com>
2025-11-23 15:16:28 +0900

    퀴즈 페이지 완료

--- react/mbti-contents-platform/src/components/test/Quiz.jsx
+++ react/mbti-contents-platform/src/components/test/Quiz.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styles from "./Quiz.module.css";
+import { Progress } from "antd";
+import { arrayShuffler } from "../../tools/tools";
 
 function Quiz({setMode, questions, mbtiScore, setMbtiScore}) {
     const [questionNum, setQuestionNum] = useState(0);
@@ -7,10 +10,6 @@ function Quiz({setMode, questions, mbtiScore, setMbtiScore}) {
         mbtiScore[type] += 1;
         console.log(mbtiScore);
         setMbtiScore( {...mbtiScore} );
-
-
-
-
         setQuestionNum((prev) => prev + 1);
     };
 
@@ -22,17 +21,24 @@ function Quiz({setMode, questions, mbtiScore, setMbtiScore}) {
     }, [questionNum, questions.length, setMode]);
 
     return <div>
-        <h3>{questions[questionNum]?.question}</h3>
-        <button
-            onClick={() => onOptionClick(questions[questionNum]?.answers[0]?.type)}
-        >
-            {questions[questionNum]?.answers[0]?.content}</button>
-        <button
-            onClick={() => onOptionClick(questions[questionNum]?.answers[1]?.type)}
-        >
-            {questions[questionNum]?.answers[1]?.content}</button>
-        <div>프로그레스 바</div>
-    </div>;
-}
+        <h3 className={styles.quesitonText}>
+            {questions[questionNum]?.question}
+        </h3>
+        {questions[questionNum]?.answers &&
+            arrayShuffler(questions[questionNum]?.answers)?.map((option) => (
+                <button
+                    className={styles.optionButton}
+                    onClick={() => onOptionClick(option.type)}            
+                    key={option.content}
+                >
+                    {option.content}
+                </button>
+        ))}
+        <Progress
+            percent={(questionNum / questions.length) * 100}
+            showInfo={false}
+        />
+    </div>
+};
 
 export default Quiz;
