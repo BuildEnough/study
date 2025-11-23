6b20735905a075ed6618aeb8139195507c122270
kbw <rhtmaehcl878@naver.com>
2025-11-23 10:08:59 +0900

    퀴즈 페이지 수정

--- react/mbti-contents-platform/src/components/test/Quiz.jsx
+++ react/mbti-contents-platform/src/components/test/Quiz.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Quiz({setMode, questions, mbtiScore, setMbtiScore}) {
     const [questionNum, setQuestionNum] = useState(0);
-    const onOptionClick = () => {
+    const onOptionClick = (type) => {
+
+        mbtiScore[type] += 1;
+        console.log(mbtiScore);
+        setMbtiScore( {...mbtiScore} );
+
+
+
+
         setQuestionNum((prev) => prev + 1);
-    }
+    };
+
+    useEffect( () => {
+        if(questionNum === questions.length) {
+            setMode("loading");
+        }
+        console.log("questionNum", questionNum);
+    }, [questionNum, questions.length, setMode]);
+
     return <div>
         <h3>{questions[questionNum]?.question}</h3>
-        <button onClick={onOptionClick}>
+        <button
+            onClick={() => onOptionClick(questions[questionNum]?.answers[0]?.type)}
+        >
             {questions[questionNum]?.answers[0]?.content}</button>
-        <button onClick={onOptionClick}>
+        <button
+            onClick={() => onOptionClick(questions[questionNum]?.answers[1]?.type)}
+        >
             {questions[questionNum]?.answers[1]?.content}</button>
         <div>프로그레스 바</div>
     </div>;
