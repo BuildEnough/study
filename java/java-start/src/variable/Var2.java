14757e51de2e16438a25c0be490cc59eb850e8d4
kbw <rhtmaehcl878@naver.com>
2025-12-04 21:46:56 +0900

    자바 연습

---
+++ java/java-start/src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+//        int a = 10;
+        int a = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("a = " + a);
+        System.out.println("a = " + a);
+    }
+}
---
+++ java/java-start/src/variable/Var8.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        byte b = 127;
+        System.out.println("b = " + b);
+
+        short s = 32767;
+        System.out.println("s = " + s);
+
+        int i = 2147483647;
+        System.out.println("i = " + i);
+
+        long l = 9223372036854775807L;
+        System.out.println("l = " + l);
+
+
+        float f = 3.14f;
+        System.out.println("f = " + f);
+
+        double d = 10.0;
+        System.out.println("d = " + d);
+    }
+}
