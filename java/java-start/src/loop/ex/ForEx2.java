ea0ecf956c3445c7533e82e9717a3e5148b79917
kbw <rhtmaehcl878@naver.com>
2025-12-09 16:41:45 +0900

    자바 연습

---
+++ java/java-start/src/loop/ex/ForEx2.java
@@ -0,0 +1,15 @@
+package loop.ex;
+
+public class ForEx2 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int num = 1; count < 10; num++) {
+            if(num % 2 == 0) {
+                System.out.println(num);
+                count++;
+            }
+        }
+
+    }
+}
